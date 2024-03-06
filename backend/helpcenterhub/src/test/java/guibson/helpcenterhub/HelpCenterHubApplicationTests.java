23e431ae39c824666dee9e7d1ee26720b9e8ee18
guibson.krause <oliveira.guibson@hotmail.com>
2024-03-06 15:20:59 -0300

    Criado esqueleto backend

---
+++ backend/helpcenterhub/src/test/java/guibson/helpcenterhub/HelpCenterHubApplicationTests.java
@@ -0,0 +1,13 @@
+package guibson.helpcenterhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpCenterHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
