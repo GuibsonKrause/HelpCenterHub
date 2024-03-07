0991da2e850b138471c1b31a7d95f33cd0ca2a34
guibson.krause <oliveira.guibson@hotmail.com>
2024-03-07 10:03:06 -0300

    Ajustado exception userNotFound

--- backend/helpcenterhub/src/main/java/guibson/helpcenterhub/domain/usecase/AuthenticateUser.java
+++ backend/helpcenterhub/src/main/java/guibson/helpcenterhub/domain/usecase/AuthenticateUser.java
@@ -4,6 +4,7 @@ import guibson.helpcenterhub.domain.entities.User;
 import guibson.helpcenterhub.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -18,21 +19,21 @@ public class AuthenticateUser {
     private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public AuthenticateUser(UserRepository userRepository, AuthenticationManager authenticationManager, PasswordEncoder passwordEncoder) {
+    public AuthenticateUser(UserRepository userRepository, AuthenticationManager authenticationManager,
+            PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.authenticationManager = authenticationManager;
         this.passwordEncoder = passwordEncoder;
     }
 
-    public User execute(String email, String password) throws AuthenticationException {
+    public User execute(String email, String password) {
         Authentication authentication = authenticationManager.authenticate(
-            new UsernamePasswordAuthenticationToken(email, password)
-        );
+                new UsernamePasswordAuthenticationToken(email, password));
         SecurityContextHolder.getContext().setAuthentication(authentication);
-        
+
         User user = userRepository.findByEmail(email);
         if (user == null) {
-            throw new AuthenticationException("User not found");
+            throw new BadCredentialsException("User not found");
         }
         return user;
     }
