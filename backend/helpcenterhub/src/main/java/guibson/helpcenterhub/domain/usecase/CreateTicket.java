df36327f945c3fcb95c25b0044063647ff9fb756
guibson.krause <oliveira.guibson@hotmail.com>
2024-03-08 11:37:04 -0300

    usando dto e mapper no usecase

--- backend/helpcenterhub/src/main/java/guibson/helpcenterhub/domain/usecase/CreateTicket.java
+++ backend/helpcenterhub/src/main/java/guibson/helpcenterhub/domain/usecase/CreateTicket.java
@@ -2,6 +2,8 @@ package guibson.helpcenterhub.domain.usecase;
 
 import guibson.helpcenterhub.domain.entities.Ticket;
 import guibson.helpcenterhub.domain.entities.TicketStatus;
+import guibson.helpcenterhub.dto.TicketDTO;
+import guibson.helpcenterhub.mapper.TicketMapper;
 import guibson.helpcenterhub.repository.TicketRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,13 +19,14 @@ public class CreateTicket {
         this.ticketRepository = ticketRepository;
     }
 
-    public Ticket execute(Long userId, String subject, String description) {
+    public TicketDTO execute(Long userId, String subject, String description) {
         Ticket newTicket = new Ticket();
         newTicket.setUserId(userId);
         newTicket.setSubject(subject);
         newTicket.setDescription(description);
         newTicket.setStatus(TicketStatus.OPEN);
         newTicket.setCreatedAt(LocalDateTime.now());
-        return ticketRepository.save(newTicket);
+        Ticket savedTicket = ticketRepository.save(newTicket);
+        return TicketMapper.INSTANCE.ticketToTicketDTO(savedTicket);
     }
 }
--- backend/helpcenterhub/src/main/java/guibson/helpcenterhub/domain/usecase/ProvideFeedback.java
+++ backend/helpcenterhub/src/main/java/guibson/helpcenterhub/domain/usecase/ProvideFeedback.java
@@ -1,6 +1,8 @@
 package guibson.helpcenterhub.domain.usecase;
 
 import guibson.helpcenterhub.domain.entities.TicketFeedback;
+import guibson.helpcenterhub.dto.TicketFeedbackDTO;
+import guibson.helpcenterhub.mapper.TicketFeedbackMapper;
 import guibson.helpcenterhub.repository.TicketFeedbackRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,13 +18,14 @@ public class ProvideFeedback {
         this.ticketFeedbackRepository = ticketFeedbackRepository;
     }
 
-    public TicketFeedback execute(Long userId, Long ticketId, int rating, String comment) {
+    public TicketFeedbackDTO execute(Long userId, Long ticketId, int rating, String comment) {
         TicketFeedback feedback = new TicketFeedback();
         feedback.setUserId(userId);
         feedback.setTicketId(ticketId);
         feedback.setRating(rating);
         feedback.setComment(comment);
         feedback.setCreatedAt(LocalDateTime.now());
-        return ticketFeedbackRepository.save(feedback);
+        TicketFeedback savedFeedback = ticketFeedbackRepository.save(feedback);
+        return TicketFeedbackMapper.INSTANCE.ticketFeedbackToTicketFeedbackDTO(savedFeedback);
     }
 }
