9e1902290504f79a52acc686b94428cb6613f9f4
guibson.krause <oliveira.guibson@hotmail.com>
2024-03-09 21:08:36 -0300

    iniciado listar ticket filtro

--- backend/helpcenterhub/src/main/java/guibson/helpcenterhub/controller/TicketController.java
+++ backend/helpcenterhub/src/main/java/guibson/helpcenterhub/controller/TicketController.java
@@ -1,8 +1,13 @@
 package guibson.helpcenterhub.controller;
 
+import guibson.helpcenterhub.domain.entities.Ticket;
 import guibson.helpcenterhub.domain.usecase.CreateTicket;
+import guibson.helpcenterhub.domain.usecase.ListTicketsByUserWithFilter;
 import guibson.helpcenterhub.dto.TicketDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,15 +16,26 @@ import org.springframework.web.bind.annotation.*;
 public class TicketController {
 
     private final CreateTicket createTicket;
+    private final ListTicketsByUserWithFilter listTicketsByUserWithFilter;
 
     @Autowired
-    public TicketController(CreateTicket createTicket) {
+    public TicketController(CreateTicket createTicket, ListTicketsByUserWithFilter listTicketsByUserWithFilter) {
         this.createTicket = createTicket;
+        this.listTicketsByUserWithFilter = listTicketsByUserWithFilter;
     }
 
     @PostMapping
     public ResponseEntity<TicketDTO> createTicket(@RequestBody TicketDTO ticketDTO) {
         TicketDTO createdTicket = createTicket.execute(ticketDTO.getUserId(), ticketDTO.getSubject(), ticketDTO.getDescription());
         return ResponseEntity.ok(createdTicket);
+    }
+
+    @GetMapping("/user/tickets")
+    public ResponseEntity<Page<Ticket>> listUserTicketsWithFilter(
+            @RequestParam Long userId,
+            @RequestParam(required = false, defaultValue = "") String filter,
+            @PageableDefault(size = 10) Pageable pageable) {
+        Page<Ticket> tickets = listTicketsByUserWithFilter.execute(userId, filter, pageable);
+        return ResponseEntity.ok(tickets);
     }
 }
