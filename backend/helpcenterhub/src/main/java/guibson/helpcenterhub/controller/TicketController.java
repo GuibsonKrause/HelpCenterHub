18f2b6d8d0348cabc8200975cad2cfcab9c5a0d2
guibson.krause <oliveira.guibson@hotmail.com>
2024-03-10 18:35:18 -0300

    implementado CloseTicket

--- backend/helpcenterhub/src/main/java/guibson/helpcenterhub/controller/TicketController.java
+++ backend/helpcenterhub/src/main/java/guibson/helpcenterhub/controller/TicketController.java
@@ -1,17 +1,24 @@
 package guibson.helpcenterhub.controller;
 
+import guibson.helpcenterhub.domain.entities.Role;
 import guibson.helpcenterhub.domain.entities.Ticket;
+import guibson.helpcenterhub.domain.entities.User;
+import guibson.helpcenterhub.domain.usecase.CloseTicket;
 import guibson.helpcenterhub.domain.usecase.CreateTicket;
 import guibson.helpcenterhub.dto.TicketDTO;
+import guibson.helpcenterhub.repository.UserRepository;
 import guibson.helpcenterhub.domain.usecase.FilterTicketsByUserIdAndDescription;
 import guibson.helpcenterhub.domain.usecase.FilterTicketsByUserIdAndTicketId;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.Authentication;
 
 @RestController
 @RequestMapping("/tickets")
@@ -22,12 +29,18 @@ public class TicketController {
     private final FilterTicketsByUserIdAndDescription filterTicketsByUserIdAndDescription;
 
     @Autowired
+    private final CloseTicket closeTicket;
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
     public TicketController(CreateTicket createTicket,
             FilterTicketsByUserIdAndTicketId filterTicketsByUserIdAndTicketId,
-            FilterTicketsByUserIdAndDescription filterTicketsByUserIdAndDescription) {
+            FilterTicketsByUserIdAndDescription filterTicketsByUserIdAndDescription, CloseTicket closeTicket) {
         this.createTicket = createTicket;
         this.filterTicketsByUserIdAndTicketId = filterTicketsByUserIdAndTicketId;
         this.filterTicketsByUserIdAndDescription = filterTicketsByUserIdAndDescription;
+        this.closeTicket = closeTicket;
     }
 
     @PostMapping
@@ -54,5 +67,25 @@ public class TicketController {
             Page<Ticket> tickets = filterTicketsByUserIdAndDescription.execute(userId, "", pageable);
             return ResponseEntity.ok(tickets);
         }
+    }
+
+    // @RequestAttribute("userId") Long managerId,
+    @PatchMapping("/{ticketId}/close")
+    public ResponseEntity<?> closeTicket(@PathVariable Long ticketId) {
+        Long managerId = (long) 3;
+        if (managerId == null || ticketId == null) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String email = authentication.getName();
+
+        User user = userRepository.findByEmail(email);
+        if (user != null && user.getRoles().contains(Role.MANAGER)) {
+            Optional<Ticket> closedTicket = closeTicket.execute(managerId, ticketId);
+            return closedTicket.map(ticket -> ResponseEntity.ok().build())
+                    .orElseGet(() -> ResponseEntity.notFound().build());
+        }
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
     }
 }
