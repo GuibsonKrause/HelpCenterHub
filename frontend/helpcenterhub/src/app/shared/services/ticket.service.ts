504bf14bd08ba84eca672f0fbc9200bc31fb8de2
guibson.krause <oliveira.guibson@hotmail.com>
2024-03-11 20:24:05 -0300

    Iniciado tela listar tickets

--- frontend/helpcenterhub/src/app/shared/services/ticket.service.ts
+++ frontend/helpcenterhub/src/app/shared/services/ticket.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Ticket } from '../models/ticket.model';
+import { environment } from '../../../../environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TicketService {
+  private apiUrl = `${environment.apiUrl}/tickets/user/`;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getTicketsByUserId(userId: number, filter?: string): Observable<Ticket[]> {
+    let params = new HttpParams();
+    if (filter) {
+      params = params.append('filter', filter);
+    }
+
+    return this.http.get<Ticket[]>(this.apiUrl + userId, { params });
+  }
 }
--- frontend/helpcenterhub/src/app/tickets/ticket-list/ticket-list.component.ts
+++ frontend/helpcenterhub/src/app/tickets/ticket-list/ticket-list.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Ticket } from '../../shared/models/ticket.model';
+import { TicketService } from '../../shared/services/ticket.service';
 
 @Component({
   selector: 'app-ticket-list',
-  standalone: true,
-  imports: [],
   templateUrl: './ticket-list.component.html',
-  styleUrl: './ticket-list.component.css'
+  styleUrls: ['./ticket-list.component.css']
 })
-export class TicketListComponent {
+export class TicketListComponent implements OnInit {
+  tickets: Ticket[] = [];
+  userId: number = 1;
+  filter: string = '';
 
+  constructor(private route: ActivatedRoute, private ticketService: TicketService) {}
+
+  ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      this.loadTickets(this.userId);
+    });
+  }
+
+  loadTickets(userId: number, filter?: string) {
+    this.ticketService.getTicketsByUserId(userId, filter)
+      .subscribe(tickets => this.tickets = tickets);
+  }
+
+  onFilterChange(filterValue: string) {
+    this.filter = filterValue;
+    this.loadTickets(this.userId, this.filter); 
+  }
 }
