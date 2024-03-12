e3a5f7bb2cb6216abb9d1d0679512a28cb618048
guibson.krause <oliveira.guibson@hotmail.com>
2024-03-12 19:21:10 -0300

    ajustado get tickets

--- backend/helpcenterhub/src/main/java/guibson/helpcenterhub/controller/TicketController.java
+++ backend/helpcenterhub/src/main/java/guibson/helpcenterhub/controller/TicketController.java
@@ -12,6 +12,8 @@ import guibson.helpcenterhub.domain.usecase.FilterTicketsByUserIdAndTicketId;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +21,8 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.Authentication;
+import java.util.Collections;
+import java.util.List;
 
 @RestController
 @RequestMapping("/tickets")
@@ -55,16 +59,23 @@ public class TicketController {
             @PathVariable Long userId,
             @RequestParam(required = false) String filter,
             @PageableDefault Pageable pageable) {
+        if (pageable == null) {
+            pageable = PageRequest.of(0, 10);
+        }
 
         if (filter != null && filter.matches("\\d+")) {
             Long ticketId = Long.parseLong(filter);
             Optional<Ticket> ticket = filterTicketsByUserIdAndTicketId.execute(userId, ticketId);
-            return ticket.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
-        } else if (filter != null) {
-            Page<Ticket> tickets = filterTicketsByUserIdAndDescription.execute(userId, filter, pageable);
-            return ResponseEntity.ok(tickets);
+            List<Ticket> ticketList = ticket.map(Collections::singletonList).orElseGet(Collections::emptyList);
+            Page<Ticket> ticketPage = new PageImpl<>(ticketList, pageable, ticketList.size());
+            return ResponseEntity.ok(ticketPage);
         } else {
-            Page<Ticket> tickets = filterTicketsByUserIdAndDescription.execute(userId, "", pageable);
+            Page<Ticket> tickets;
+            if (filter != null) {
+                tickets = filterTicketsByUserIdAndDescription.execute(userId, filter, pageable);
+            } else {
+                tickets = filterTicketsByUserIdAndDescription.execute(userId, "", pageable);
+            }
             return ResponseEntity.ok(tickets);
         }
     }
