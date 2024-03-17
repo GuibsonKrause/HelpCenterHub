d80f0b558b7d50fc910ad35662d0b07ef3e4c288
guibson.krause <oliveira.guibson@hotmail.com>
2024-03-16 22:44:45 -0300

    adicionado tela de login como principal

--- frontend/helpcenterhub/src/app/app.routes.ts
+++ frontend/helpcenterhub/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TicketListComponent } from './tickets/ticket-list/ticket-list.component';
 import { HttpClientModule } from '@angular/common/http';
+import { LoginComponent } from './auth/login/login.component';
 
 export const routes: Routes = [
-  { path: '', component: TicketListComponent }, 
+  { path: '', component: LoginComponent }, 
+  //{ path: '/ticketlist', component: TicketListComponent }, 
   // ...other routes 
 ];
 
