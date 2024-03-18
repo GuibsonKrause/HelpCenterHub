bd258342bdc7713d4b6a81ed8ad02040963c1c5c
guibson.krause <oliveira.guibson@hotmail.com>
2024-03-18 01:53:21 -0300

    ajustado metodo depreciado

--- frontend/helpcenterhub/src/app/auth/auth.guard.ts
+++ frontend/helpcenterhub/src/app/auth/auth.guard.ts
@@ -1,26 +1,22 @@
-// auth.guard.ts
-import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { inject, Injectable } from '@angular/core';
+import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { AuthService } from '../shared/services/auth.service';
 import { Observable } from 'rxjs';
 import { map, take } from 'rxjs/operators';
-import { AuthService } from '../shared/services/auth.service';
 
 @Injectable({
   providedIn: 'root',
 })
-export class AuthGuard implements CanActivate {
-  constructor(private authService: AuthService, private router: Router) {}
+export class AuthGuard {
+  private authService = inject(AuthService);
+  private router = inject(Router);
 
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot
-  ): Observable<boolean> | Promise<boolean> | boolean {
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> {
     return this.authService.isLoggedIn$.pipe(
       take(1),
       map((isLoggedIn: boolean) => {
         if (!isLoggedIn) {
-          this.router.navigate(['']);
-          return false;
+          return this.router.createUrlTree(['']);
         }
         return true;
       })
