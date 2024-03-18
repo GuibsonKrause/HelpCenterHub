fb6c6c7fe8f022199c6239f09c1ad38063422863
guibson.krause <oliveira.guibson@hotmail.com>
2024-03-18 05:05:43 -0300

    implementado getTicket para manager

--- backend/helpcenterhub/src/main/java/guibson/helpcenterhub/controller/UserInfoController.java
+++ backend/helpcenterhub/src/main/java/guibson/helpcenterhub/controller/UserInfoController.java
@@ -2,11 +2,11 @@ package guibson.helpcenterhub.controller;
 
 import guibson.helpcenterhub.domain.entities.User;
 import guibson.helpcenterhub.dto.UserDTO;
-import guibson.helpcenterhub.repository.UserRepository;
+import guibson.helpcenterhub.service.UserService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,24 +17,18 @@ import java.util.stream.Collectors;
 @RequestMapping("/userinfo")
 public class UserInfoController {
 
-    private final UserRepository userRepository;
+    private final UserService userService;
 
     @Autowired
-    public UserInfoController(UserRepository userRepository) {
-        this.userRepository = userRepository;
+    public UserInfoController(UserService userService) {
+        this.userService = userService;
     }
 
     @GetMapping
     public UserDTO getUserInfo() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        String email = null;
 
-        if (authentication.getPrincipal() instanceof OAuth2User) {
-            OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
-            email = oAuth2User.getAttribute("email");
-        }
-
-        User user = userRepository.findByEmail(email).orElseThrow(() -> new RuntimeException("User not found"));
+        User user = userService.getUserFromAuthentication(authentication);
 
         UserDTO userDTO = new UserDTO();
         userDTO.setId(user.getId());
